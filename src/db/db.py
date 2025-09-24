6527c0467c90c43ab221e9a2b5f5625e8cd6684f
async1o <matvey.gusak2000@mail.ru>
2025-09-24 20:45:08 +0300

    docker compose

--- src/db/db.py
+++ src/db/db.py
@@ -19,7 +19,7 @@ def get_redis() -> Redis:
 
 async def reset_tables():
     redis = get_redis()
-    redis.flushall()
+    await redis.flushall()
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
